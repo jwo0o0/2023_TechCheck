ec0f98085ef70f10fafc715bfff01ec96ea5afef
jwo0o0 <jwo0o0358@gmail.com>
2023-11-27 14:35:18 +0900

    add mobile height size setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import Head from "next/head";
 import LandingPage from "@/components/landing/LandingPage";
+import { useEffect } from "react";
 
 export default function Home() {
+  //mobile height size 설정
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  }, []);
+
   return (
     <>
       <Head>
